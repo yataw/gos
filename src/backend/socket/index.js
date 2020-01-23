1bd551695c52120153a2f98d7422d4590be2bd90
Yakov Kovalev <yataw@ya.ru>
2020-01-23 10:36:51 +0300

    add history

--- src/backend/socket/index.js
+++ src/backend/socket/index.js
@@ -7,6 +7,8 @@ import Game from '../../game/game';
 module.exports = function (server, log) {
     const io = require('socket.io')(server, {logger: log});
 
+    const history = [];
+
     io.set('origins', '*:*');
     io.on(Events.connection, socket => {
         // TODO authorization
@@ -14,10 +16,17 @@ module.exports = function (server, log) {
         socket.on(Events.setname, ({name}) => {
             const color = '000';
 
+            history.forEach(({text, author}) => {
+                const id = Math.random();
+
+                socket.emit(Events.chat_message, {id, text, author, me: false});
+            })
             // game.addPlayer({id: socket.id, socket, name, color});
 
             socket.on(Events.chat_message, text => {
                 const id = Math.random();
+
+                history.push({text, author: name});
 
                 socket.broadcast.emit(Events.chat_message, {id, text, author: name, me: false});
                 socket.emit(Events.chat_message, {id, text, author: name, me: true});
